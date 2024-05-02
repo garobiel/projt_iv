c619fcd4e5043ad1ae0250171d4f5ca45699438f
Gabriel Rocha <gabriellvsero@gmail.com>
2024-05-02 14:23:18 -0300

    Nome da empresa errado

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", async(req, res) => {
-    res.send("Página inicial CNPAT ");
+    res.send("Página inicial CONPAT ");
 
 });
 
