884c3be00fa5df36e59bf47bf208b17e8a56fa57
Gabriel Rocha <gabriellvsero@gmail.com>
2024-05-03 00:40:36 -0300

    module exports

---
+++ models/db.js
@@ -0,0 +1,19 @@
+// Aqui estamos pegando dentro do modulo a classe sequelize e tambem estamos importando a biblioteca sequelize
+const  Sequelize  = require('sequelize');
+
+//Aqui vamos criar instancias(instancia e um objeto especifico criado aparti de uma classe ou de um contrutor)
+const sequelize = new Sequelize( "conpat", "root","Amoskate123*", {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+sequelize.authenticate()
+.then(function(){
+    console.log("Conxexão com o banco de dados estabelecida com sucesso!");
+}).catch(function(){
+    console.log("Erro: Conexão com o banco de dados não realizada com sucesso!");
+});
+
+module.exports = sequelize; 
+
+
