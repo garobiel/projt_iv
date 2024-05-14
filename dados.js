af3fe88cd9d18c604df201090148b3e636309440
Gabriel Rocha <gabriellvsero@gmail.com>
2024-05-14 12:13:54 -0300

    AT

--- dados.js
+++ dados.js
@@ -1,8 +1,10 @@
 const connection = require('./main') //Importa a configuração do banco de dados
 
-connection.query('CREATE TABLE Controle_iv (
+connection.query(`CREATE TABLE Controle_iv (
     id intger primary key autoincrement,
+    Colaborador TEXT,
+    Patrimonio VARCHAR(6)
 
 )
     
-') 
+`) 
