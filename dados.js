8d84f3bb8bae77a03e7ed0de28fc47d5b603da16
Gabriel Rocha <gabriellvsero@gmail.com>
2024-05-14 16:20:59 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,10 +1,27 @@
-const connection = require('./main') //Importa a configuração do banco de dados
+const connection = require('./main'); // Importa a configuração do banco de dados
+const mysql = require('mysql2/promise');
 
-connection.query(`CREATE TABLE Controle_iv (
-    id intger primary key autoincrement,
-    Colaborador TEXT,
-    Patrimonio VARCHAR(6)
-
-)
-    
-`) 
+// Estabelece uma conexão com o banco de dados usando a configuração importada
+mysql.createConnection(connection)
+    .then(async conn => {
+        // Realiza a consulta para criar a tabela
+        await conn.query(`
+            CREATE TABLE IF NOT EXISTS Controle_iv (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                Colaborador TEXT,
+                Patrimonio VARCHAR(6),
+                Produto TEXT,
+                Marca TEXT,
+                Serie TEXT,
+                Modelo TEXT,
+                Entrada TEXT,
+                Saida TEXT
+            )
+        `);
+        console.log('Tabela criada com sucesso!');
+        // Não se esqueça de fechar a conexão quando terminar de usar
+        conn.end();
+    })
+    .catch(error => {
+        console.error('Erro ao conectar ao banco de dados:', error);
+    });
