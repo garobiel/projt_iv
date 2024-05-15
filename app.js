c53ba6db3a45ecd06a332852736d3f801fe2e455
Gabriel Rocha <gabriellvsero@gmail.com>
2024-05-15 16:36:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
+// app.js
+
 const express = require('express');
 const app = express();
-
 const db = require('./models/db');
+const path = require('path');
+const pool = require('./dados')
 
-app.get("/", async(req, res) => {
-    res.send("Página inicial - CONPAT ");
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
+app.get('/', async (req, res) => {
+    try {
+        // Consulta ao banco de dados para obter os dados
+        const dados = await pool.show(); // Suponha que obterDados() seja uma função que retorna os dados do banco de dados
+
+        // Renderiza a visualização com os dados
+        res.render('index', { dados });
+    } catch (error) {
+        console.error('Erro ao obter dados do banco de dados:', error);
+        res.status(500).send('Erro ao recuperar os dados do banco de dados');
+    }
 });
 
 app.listen(8080, () => {
-    console.log("Servidor iniciado na porta 8080: http://localhost:8080")
-
+    console.log('Servidor rodando na porta 8080');
 });
