0a304fffd76ea9f0a04ba8e3a786aebcdbd3c71a
Gabriel Rocha <gabriellvsero@gmail.com>
2024-05-15 16:37:10 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -3,7 +3,7 @@ const pool = require('./main');
 pool.getConnection()
     .then(async conn => {
         await conn.query(`
-            CREATE TABLE IF NOT EXISTS controle_iventario_2 (
+            CREATE TABLE IF NOT EXISTS Controle_iv (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 Colaborador TEXT,
                 Patrimonio VARCHAR(6),
@@ -15,30 +15,36 @@ pool.getConnection()
                 Saida TEXT
             )
         `);
-          
-        await conn.query(`
-        INSERT INTO controle_iventario_2 (
-                Colaborador, 
-                Patrimonio,
-                Produto,
-                Marca,
-                Serie,
-                Modelo,
-                Entrada,
-                Saida
-            ) VALUES("Allan", "001001", "CPU", "DELL", "BRG12345", "T3610", "E", "-")
-           
-        `);
-        const select = await conn.query(`
-        SELECT * FROM controle_iventario_2
+        module.exports = {
+            async show() {
+                const select = await conn.query(`
+        SELECT * FROM controle_iv
         `)
-        console.log(select)
+                console.log(select)
+            }
+        }
 
-        
+        /*
+                
+                await conn.query(`
+                INSERT INTO controle_iv (
+                        Colaborador, 
+                        Patrimonio,
+                        Produto,
+                        Marca,
+                        Serie,
+                        Modelo,
+                        Entrada,
+                        Saida
+                    ) VALUES("Allan", "001001", "CPU", "DELL", "BRG12345", "T3610", "E", "-")
+                   
+                `);
+                */
         console.log('Dados inseridos com sucesso!');
         conn.release(); // Liberar a conexão de volta para o pool
     })
     .catch(error => {
         console.error('Erro ao conectar ao banco de dados:', error);
     });
- 
+
+
