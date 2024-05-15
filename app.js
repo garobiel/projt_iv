85bbe3dbabb204cd99ef914fcaf6f0d6d85136a7
Gabriel Rocha <gabriellvsero@gmail.com>
2024-05-15 16:31:51 -0300

    at

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
+// app.js
+
 const express = require('express');
 const app = express();
-const conectdados = require('./dados')
 const db = require('./models/db');
-const rots = express.Router()
-app.set('view engine', 'ejs')
+const path = require('path');
+const pool = require('./dados')
 
-rots.get("/", async(req, res) => {
-    res.redirect('')
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
+app.get('/', async (req, res) => {
+    try {
+        // Consulta ao banco de dados para obter os dados
+        const dados = await pool.show(); // Suponha que obterDados() seja uma função que retorna os dados do banco de dados
+
+        // Renderiza a visualização com os dados
+        res.render('index', { dados });
+    } catch (error) {
+        console.error('Erro ao obter dados do banco de dados:', error);
+        res.status(500).send('Erro ao recuperar os dados do banco de dados');
+    }
 });
 
 app.listen(8080, () => {
-    console.log("Servidor iniciado na porta 8080: http://localhost:8080")
-
+    console.log('Servidor rodando na porta 8080');
 });
-
-module.exports = rots;
