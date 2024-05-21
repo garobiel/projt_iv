5e89930c76b880befa45409f3b3341ff889ddce5
Gabriel Rocha <gabriellvsero@gmail.com>
2024-05-20 21:59:16 -0300

    at

--- dados.js
+++ dados.js
@@ -2,6 +2,7 @@ const pool = require('./main');
 
 pool.getConnection()
     .then(async conn => {
+        try { 
         await conn.query(`
             CREATE TABLE IF NOT EXISTS Controle_iv (
                 id INT AUTO_INCREMENT PRIMARY KEY,
@@ -10,41 +11,42 @@ pool.getConnection()
                 Produto TEXT,
                 Marca TEXT,
                 Serie TEXT,
-                Modelo TEXT,
-                Entrada TEXT,
-                Saida TEXT
+                Modelo TEXT
+              
             )
         `);
-    module.exports = {
-        async show() {
-            const select = await conn.query(`
-        SELECT * FROM controle_iv
-        `)
-            console.log(select)
-        }
-    }
-
-/*
-        
         await conn.query(`
-        INSERT INTO controle_iv (
-                Colaborador, 
+            INSERT INTO controle_iv (
+                Colaborador,
                 Patrimonio,
                 Produto,
                 Marca,
                 Serie,
-                Modelo,
-                Entrada,
-                Saida
-            ) VALUES("Allan", "001001", "CPU", "DELL", "BRG12345", "T3610", "E", "-")
-           
+                Modelo
+            
+            ) VALUES("Gabriel", "001001", "CPU", "DELL", "BRG12345", "T3610") 
+        
         `);
-        */
+
         console.log('Dados inseridos com sucesso!');
-        conn.release(); // Liberar a conexão de volta para o pool
-    })
-    .catch(error => {
-        console.error('Erro ao conectar ao banco de dados:', error);
-    });
+
+        //Funcao para exibir os dados
+        const show = async () => {
+            const [rows] = await conn.query('SELECT * FROM controle_iv');
+            console.log(rows);
+
+        };
+
+        await show(); //Chama a funcao para exibir os dados
+
+        conn.release(); //Liberar a conexao de volta para o pool
+    } catch (error) {
+        console.error('Erro ao executar a query:', error);
+        conn.release(); //Certifique-se de liberar a conexao em caso de erro
+    }
+})
+.catch(error => {
+    console.error('Erro ao conectar ao banco de dados')
+}); 
  
 
